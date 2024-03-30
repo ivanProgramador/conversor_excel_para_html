32f33ee7710d72831fc8e52e073a25c9ba3293d8
ivan <ivan-guita@hotmail.com>
2024-03-30 08:50:58 -0300

    fs transformada em função assicncrona

--- conversor/Reader.js
+++ conversor/Reader.js
@@ -1,15 +1,37 @@
 const fs = require("fs");
+const util = require("util");
 
+/*
+fs é um biblioteca antiga criada antes da criação das promisses,
+nesse caso eu não posso usar ela como função assincrona diretamente 
+eu tenho que fazer a coversão usando a biblioteca
+
+util que tem a função promissify
+
+*/
 class Reader{
-    Read(filepath){
-        fs.readFile(filepath,{encoding:"utf-8"},(err,data)=>{
-            if(err){
-                console.log(err);
-            }else{
-                console.log(data);
-            }
-        })
+
+    constructor(){
+        //convertendo
+        this.reader = util.promisify(fs.readFile);
+
     }
+
+   //usando
+   async Read(filepath){
+       try{
+
+         return await this.reader(filepath,{encoding:"utf-8"});
+         
+       }catch(err){
+
+         return undefined
+
+       }
+    }
+
+
+
 }
 
 module.exports = Reader;
