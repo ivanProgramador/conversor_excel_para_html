f15b43ab667ae0ec4eb587d67689d63db860a7d2
ivan <ivan-guita@hotmail.com>
2024-03-30 09:25:59 -0300

    classe de leitura pronta fim da aula 180

--- conversor/app.js
+++ conversor/app.js
@@ -2,4 +2,20 @@ var Reader = require("./Reader");
 
 var leitor = new Reader();
 
-leitor.Read("./lista_de_alunos.csv")
+/*
+ o método Read agora é assincrono então para executar ele eu vou precisar 
+ executar de forma assincrona 
+*/
+
+async function main(){
+
+   //agora ele espera o retono da função pra depois mostrar os dados no console
+   var dados =  await leitor.Read("./lista_de_alunos.csv");
+   
+   console.log(dados);
+
+
+}
+
+main();
+
