39ae550d2b76864383161ca96c74f16b441aa70a
ivan <ivan-guita@hotmail.com>
2024-03-31 18:54:10 -0300

    função writer ja esta gerando o html final da aula 185

--- conversor/app.js
+++ conversor/app.js
@@ -1,8 +1,13 @@
 var Reader = require("./Reader");
 var Processor = require("./Processor");
+var Writer = require("./Writer");
 var Table = require("./Table");
 var HtmlParser = require("./HtmlParser");
+
+
 var leitor = new Reader();
+var escritor = new Writer();
+
 
 async function main(){
 
@@ -12,9 +17,16 @@ async function main(){
 
     var usuarios = new Table(dadosProcessados);
 
-   var html = await HtmlParser.parse(usuarios);
+    var html = await HtmlParser.parse(usuarios);
 
-   console.log(html);
+    // pra escrever um novo arquivo html eu tenho que passar o nome dele 
+    //e depois os dados que vão ser escritos para que todos so arquivos não fiquem com o mesmo  nome 
+    // eu chamei a função date now que conta data/hora/minutos/segundos então cada vez que eu 
+    // gerar um aruivo novo o nome vai mudar    
+
+    escritor.Write( Date.now()+".html",html);
+
+    
 
 
 }
